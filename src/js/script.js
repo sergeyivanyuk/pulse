3431a9e817345d91c135b2dc18ec2e2aaa16e824
Sergey <sergeyivanyuk89@gmail.com>
2021-02-05 11:24:00 +0300

    add slider arrows

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-right.svg"></button>',
         responsive: [
             {
               breakpoint: 992,
