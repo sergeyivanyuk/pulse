5eab41b570dc7fd644c50a3ea2749572349717c5
Sergey <sergeyivanyuk89@gmail.com>
2021-02-05 11:20:19 +0300

    carousel responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
-        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-right.png"></button>',
         responsive: [
