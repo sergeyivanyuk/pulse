1297f939b267e090ee519ee157b7a73e622bf079
Sergey <sergeyivanyuk89@gmail.com>
2021-02-06 13:21:38 +0300

    add validate forms

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // slider
     $('.carousel__inner').slick({
         speed: 1000,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.svg"></button>',
@@ -15,12 +16,15 @@ $(document).ready(function(){
           ]
     }
     );
-        
+
+
+    // tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
       $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
 
     function toggleSlide(item) {
       $(item).each(function(i) {
@@ -34,4 +38,47 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // modal
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
+    
+    // validate
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: 'required',
+          phone: 'required',
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: "пожалуйста введите свое имя",
+          phone: "введите телефон",
+          email: {
+            required: "введите почту",
+            email: "неправильный формат почты"
+          }
+        }
+      });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+    validateForms('#thanks form');
 });
+    
