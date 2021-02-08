5eae457eacfde9ea316673bde4497fe77a78f037
Sergey <sergeyivanyuk89@gmail.com>
2021-02-09 01:30:24 +0300

    add mailer

--- src/js/script.js
+++ src/js/script.js
@@ -83,5 +83,22 @@ $(document).ready(function(){
 
     // mask
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // mail
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: 'mailer/smart.php',
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $()
+
+        $('form').trigger('reset');
+      });
+      return false
+    });
 });
     
