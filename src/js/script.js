f8e04af83e883cc5b477dfd8b03d922465b66c46
Sergey <sergeyivanyuk89@gmail.com>
2021-02-10 02:05:15 +0300

    add script scroll uppage

--- src/js/script.js
+++ src/js/script.js
@@ -100,5 +100,21 @@ $(document).ready(function(){
       });
       return false;
     });
+
+    // scroll pageup
+
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
 });
     
